f31bd2b4fef1f1eae01533ee138b4f7c60a32319
maccony202 <73612968+maccony202@users.noreply.github.com>
2022-12-21 15:24:24 +0000

    Add modules and implemented add and remove to Todo list

--- src/index.js
+++ src/index.js
@@ -1,57 +1,28 @@
 // import _ from 'lodash';
 import './style.css';
+import displayTodoTask from './modules/myFunction.js';
 
-const taskList = document.querySelector('.task-list');
+window.addEventListener('load', () => {
+  const todos = JSON.parse(localStorage.getItem('todos')) || [];
+  const TodoForm = document.querySelector('#todo-form');
 
-const myTodoTask = [
-  {
-    discription: 'I will finish my project today',
-    isComplete: false,
-    index: 0,
-  },
+  TodoForm.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-  {
-    discription: 'I will go and watch a football match',
-    isComplete: false,
-    index: 1,
-  },
-  {
-    discription: 'I will update my Linkdin profile',
-    isComplete: false,
-    index: 2,
-  },
+    const todo = {
+      contents: e.target.elements.content.value,
+      done: false,
+      id: todos.length + 1,
+    };
 
-  {
-    discription: 'I will go to the gym',
-    isComplete: false,
-    index: 3,
-  },
+    todos.push(todo);
 
-  {
-    discription: 'I will drink cofee',
-    isComplete: false,
-    index: 4,
-  },
-];
+    localStorage.setItem('todos', JSON.stringify(todos));
 
-const displayTodoTask = ({
-  discription,
-}) => {
-  const ul = document.createElement('ul');
-  ul.className = 'task-items';
-  ul.innerHTML = `
-    <li><input type="checkbox"><p>${discription}</p><i class="bi bi-three-dots-vertical"></i></li>
-    <hr>
-   
-    `;
+    e.target.reset();
 
-  return (ul);
-};
-
-const getTodoTask = () => {
-  myTodoTask.forEach((myObject, index) => {
-    taskList.append(displayTodoTask(myObject, index));
+    displayTodoTask();
   });
-};
 
-getTodoTask();
+  displayTodoTask();
+});
