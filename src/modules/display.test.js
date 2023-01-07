43d2ba5bcf1e8632e3f1ee8f9cd32cb5b1ddac2f
maccony202 <73612968+maccony202@users.noreply.github.com>
2023-01-07 21:30:36 +0000

    Add other testing functions

--- src/modules/display.test.js
+++ src/modules/display.test.js
@@ -2,7 +2,9 @@
  * @jest-environment jsdom
  */
 
-import { addNewList, removeList } from './display.js';
+import {
+  addNewList, removeList, editTask, notCompleted, completed,
+} from './display.js';
 
 describe('Testing remove and add functions', () => {
   test('addNewList adds a new task to the todos list', () => {
@@ -41,5 +43,42 @@ describe('Testing remove and add functions', () => {
       description: 'Buy milk',
       completed: false,
     }]);
+  });
+
+  // test for edit
+  test('notCompleted sets the completed property of the todo with the specified id to false', () => {
+    const todos = [{ id: 1, text: 'todo 1', completed: false }, { id: 2, text: 'todo 2', completed: true }];
+    localStorage.setItem('todos', JSON.stringify(todos));
+    notCompleted(1);
+    const updatedTodos = JSON.parse(localStorage.getItem('todos'));
+    expect(updatedTodos[0].completed).toBe(false);
+  });
+
+  describe('editTask', () => {
+    it('updates the description of the task with the given id', () => {
+      const id = 0;
+      const value = 'updated task';
+      const todos = [{ id: 0, description: 'updated task' }];
+
+      editTask(id, value);
+
+      expect(todos[id].description).toEqual(value);
+    });
+  });
+
+  describe('completed', () => {
+    it('marks the task with the given id as completed', () => {
+      const id = 0;
+      const todos = [{ id: 0, completed: true }];
+      const localStorageMock = {
+        getItem: jest.fn().mockReturnValue(JSON.stringify(todos)),
+        setItem: jest.fn(),
+      };
+      global.localStorage = localStorageMock;
+
+      completed(id);
+
+      expect(todos[id].completed).toEqual(true);
+    });
   });
 });
